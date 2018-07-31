667e13b3b4b7ea024fe4d1099d7c9b31e6298bbe
jalen <jalenparham97@gmail.com>
2018-07-31 09:00:18 -0400

    found and fixed save button bug

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,18 @@
 "use strict";
+
 $(document).ready(() => {
+  // Declare Saved Seat
+  let saved_seat;
+  // Click event for seat and X buttons
   $("body").on("click", ".floor__plan--container .available, .delete", (e) => {
-    let saved_seat = $(e.target);
-    $(".table__number").text(`Table Number: ${$(e.target).text()}`);
-    $(".form").fadeToggle();
+    saved_seat = $(e.target);
+    $(".table__number").text(`Table Number: ${saved_seat.text()}`);
+    $(".form").fadeToggle(1000);
+    // Click event for save button
     $("body").on("click", ".form .form__button", (e) => {
-        $(saved_seat).addClass("reserved");
-        $(saved_seat).removeClass("available");
-        $(".form").fadeOut(1000);
+      saved_seat.addClass("reserved");
+      saved_seat.removeClass("available");
+      $(".form").fadeOut(1000);
     });
   });
 });
