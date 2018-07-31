f6aa4e4f53111bfc3374041529a98aa3263c4e4c
jalen <jalenparham97@gmail.com>
2018-07-31 09:40:51 -0400

    cleaned up sass

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,12 @@ $(document).ready(() => {
   // Declare Saved Seat
   let saved_seat;
   // Click event for seat and X buttons
-  $("body").on("click", ".floor__plan--container .available, .delete", (e) => {
+  $("body").on("click", ".floor__plan--container .available, .form .delete", (e) => {
     saved_seat = $(e.target);
     $(".table__number").text(`Table Number: ${saved_seat.text()}`);
     $(".form").fadeToggle(1000);
     // Click event for save button
-    $("body").on("click", ".form .form__button", (e) => {
+    $("body").on("click", ".form .form__button", () => {
       saved_seat.addClass("reserved");
       saved_seat.removeClass("available");
       $(".form").fadeOut(1000);
